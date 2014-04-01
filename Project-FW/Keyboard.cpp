4ef7b9319dd43388fc3cf533466bac4d880e80e0
Nickchooshin <gustncjswp95@gmail.com>
2014-04-02 02:55:10 +0900

    Add comment in Keyboard.cpp

--- Project-FW/Keyboard.cpp
+++ Project-FW/Keyboard.cpp
@@ -25,25 +25,33 @@ Keyboard::~Keyboard()
 
 bool Keyboard::Init()
 {
+	// 키보드 DirectInputDevice 초기화
 	//if( FAILED( m_pDInput->CreateDevice( GUID_SysKeyboard, &m_pDIDKeyboard, NULL ) ) )
 	if( FAILED( g_InputDevice->m_pDInput->CreateDevice( GUID_SysKeyboard, &m_pDIDKeyboard, NULL ) ) )
 	{
 		MessageBox(NULL, "Keyboard CreateDevice Fail", "Error", MB_OK) ;
 		return false ;
 	}
+
+	// 키보드 DirectInputDevice 포멧 설정
 	if( FAILED( m_pDIDKeyboard->SetDataFormat( &c_dfDIKeyboard ) ) )
 	{
 		MessageBox(NULL, "Keyboard SetDataFormat Fail", "Error", MB_OK) ;
 		return false ;
 	}
+
+	// 키보드 DirectInputDevice 의 액세스 권한을 설정
+	// DISCL_EXCLUSIVE (독점모드) - 다른 애플리케이션으로 포커스 이동이 안된다. 마우스와 키보드를 독점모드로 사용하게 될 경우 윈도우 메뉴, 이동, 크기 변경을 할 수 없다. DISCL_BACKGROUND와 같이 쓰일 수 없다.
+	// DISCL_FOREGROUND - 포커스를 가지고 있지 않을 경우 디바이스의 데이터(상태)를 얻지 못 한다.
 	//if( FAILED( m_pDIDKeyboard->SetCooperativeLevel( hWnd, DISCL_EXCLUSIVE | DISCL_FOREGROUND ) ) )
 	if( FAILED( m_pDIDKeyboard->SetCooperativeLevel( g_InputDevice->m_hWnd, DISCL_EXCLUSIVE | DISCL_FOREGROUND ) ) )
 	{
 		MessageBox(NULL, "KeyBoard SetCooperativeLevel Fail", "Error", MB_OK) ;
 		return false ;
 	}
-	m_pDIDKeyboard->Acquire() ;
-	m_pDIDKeyboard->GetDeviceState(sizeof(KeyBuffer), &KeyBuffer) ;
+
+	m_pDIDKeyboard->Acquire() ;										// 키보드 DirectInputDevice 습득
+	m_pDIDKeyboard->GetDeviceState(sizeof(KeyBuffer), &KeyBuffer) ;	// 키보드 DirectInputDevice 에서 키보드의 상태를 가져온다
 }
 
 bool Keyboard::Update()
@@ -51,6 +59,8 @@ bool Keyboard::Update()
 	HRESULT hr ;
 	ZeroMemory(KeyBuffer, sizeof(KeyBuffer)) ;
 
+	// 키보드 디바이스로부터 키보드의 상태를 가져올 수 없으면
+	// 키보드의 디바이스를 다시 습득한다
 	if( FAILED( m_pDIDKeyboard->GetDeviceState( sizeof(KeyBuffer), &KeyBuffer ) ) )
 	{
 		hr = m_pDIDKeyboard->Acquire() ;
