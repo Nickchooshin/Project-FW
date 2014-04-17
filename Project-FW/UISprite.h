a468eb96aafd5b316235ef678c3a3800e60a3759
Nickchooshin <gustncjswp95@gmail.com>
2014-04-18 00:46:02 +0900

    Add Simple UISprite Class

---
+++ Project-FW/UISprite.h
@@ -0,0 +1,46 @@
+#pragma once
+#include <d3dx9.h>
+
+class CUISprite
+{
+private :
+	LPDIRECT3DVERTEXBUFFER9	m_pVB ;
+	LPDIRECT3DINDEXBUFFER9	m_pIB ;
+	LPDIRECT3DTEXTURE9		m_pTexture ;
+
+	float m_fWidth, m_fHeight ;
+	float m_fX, m_fY ;
+	int m_R, m_G, m_B ;
+	int m_nAlpha ;
+	float m_tu[4], m_tv[4] ;
+public :
+	static LPDIRECT3DDEVICE9 pd3dDevice ;
+
+public :
+	CUISprite() ;
+	~CUISprite() ;
+
+	bool Init(float Width, float Height, char *texfile) ;
+
+	void SetXY(float X, float Y) ;						// 좌표설정 (중앙 기준)
+	void SetRGB(int R, int G, int B) ;					// RGB값 설정
+	void SetAlpha(int Alpha) ;							// 알파값 설정 0~255
+
+	void TexReverse() ;									// 텍스쳐 뒤집기
+
+	void Render() ;
+private :
+	HRESULT InitVB() ;
+
+	bool SetTexture(char *texfile) ;
+	void SetupMatrices() ;
+} ;
+
+struct UISPRITE_VERTEX
+{
+	float		x, y, z, rhw ;	// The position
+	D3DCOLOR	color ;			// The color
+	float		tu, tv ;		// The texture coordinates (x, y)
+} ;
+
+#define D3DFVF_UISPRITE_VERTEX (D3DFVF_XYZRHW | D3DFVF_DIFFUSE | D3DFVF_TEX1)
