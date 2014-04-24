f7af799a4cadffbc06bec5998f78591b0ddc5744
Nickchooshin <gustncjswp95@gmail.com>
2014-04-25 04:05:49 +0900

    CCamera 클래스 좌표 변수 D3DXVECTOR2로 변경, SetXY 함수 이름 변경, 직교투영 왼쪽 상단을 0,0으로 둠

--- Project-FW/Camera.cpp
+++ Project-FW/Camera.cpp
@@ -4,18 +4,17 @@ LPDIRECT3DDEVICE9 CCamera::pd3dDevice = NULL ;
 float CCamera::WinWidth = 0.0f ;
 float CCamera::WinHeight = 0.0f ;
 
-CCamera::CCamera() : m_fX(0.0f),
-					 m_fY(0.0f)
+CCamera::CCamera() : m_Position(0.0f, 0.0f)
 {
 }
 CCamera::~CCamera()
 {
 }
 
-void CCamera::SetXY(float x, float y)
+void CCamera::SetPosition(float x, float y)
 {
-	m_fX = x ;
-	m_fY = y ;
+	m_Position.x = x ;
+	m_Position.y = y ;
 }
 
 void CCamera::Run()
@@ -24,14 +23,15 @@ void CCamera::Run()
 	D3DXMatrixIdentity( &matWorld );
 	pd3dDevice->SetTransform( D3DTS_WORLD, &matWorld ) ;
 
-	D3DXVECTOR3 vEyePt( m_fX, m_fY, -10.0f );	// 카메라의 위치
-	D3DXVECTOR3 vLookatPt( m_fX, m_fY, 0.0f );	// 카메라의 시선
-	D3DXVECTOR3 vUpVec( 0.0f, 1.0f, 0.0f );	// 상방벡터 (정수리)
-	D3DXMATRIXA16 matView;
-	D3DXMatrixLookAtLH( &matView, &vEyePt, &vLookatPt, &vUpVec );
+	D3DXVECTOR3 vEyePt( m_Position.x, m_Position.y, -10.0f ) ;	// 카메라의 위치
+	D3DXVECTOR3 vLookatPt( m_Position.x, m_Position.y, 0.0f ) ;	// 카메라의 시선
+	D3DXVECTOR3 vUpVec( 0.0f, 1.0f, 0.0f ) ;					// 상방벡터 (정수리)
+	D3DXMATRIXA16 matView ;
+	D3DXMatrixLookAtLH( &matView, &vEyePt, &vLookatPt, &vUpVec ) ;
 	pd3dDevice->SetTransform( D3DTS_VIEW, &matView ) ;
 
 	D3DXMATRIXA16 matProj;
-	D3DXMatrixOrthoLH(&matProj, WinWidth, WinHeight, 1.0f, 1000.0f) ;
-	pd3dDevice->SetTransform( D3DTS_PROJECTION, &matProj );
+	//D3DXMatrixOrthoLH(&matProj, WinWidth, WinHeight, -100.0f, 100.0f) ;
+	D3DXMatrixOrthoOffCenterLH(&matProj, 0.0f, WinWidth, WinHeight, 0.0f, -100.0f, 100.0f) ;
+	pd3dDevice->SetTransform( D3DTS_PROJECTION, &matProj ) ;
 }
